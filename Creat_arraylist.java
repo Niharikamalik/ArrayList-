6511ca8ca99f4725c3cde398bd452e38740ddda2
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-10-25 21:17:26 +0530

    Create Creat_arraylist.java

---
+++ Creat_arraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer> al =new ArrayList<Integer>();
+	    al.add(12);
+	    al.add(24);
+	    al.add(54);
+	    al.add(25);
+	    System.out.print(al+" ");
+	}
+}
