4cf0eb22f980fa18635e972f2f5f4cc15339bbdb
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-10-25 22:09:00 +0530

    Create ArrayList_methods.java

---
+++ ArrayList_methods.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer> al =new ArrayList<Integer>();
+	    al.add(12);
+	    al.add(24);
+	    al.add(54);
+	    al.add(2,25);
+	    System.out.println(al+" ");
+	    System.out.println(al.contains(24));
+	    al.remove(2);
+	    System.out.println(al);
+	    System.out.println(al.get(2));
+	    al.set(1,56);
+	    System.out.println(al);
+	    System.out.println(al.indexOf(4));
+	    System.out.println(al.lastIndexOf(54));
+	}
+}
